2a055042f78c395843523d702d26b1b63eebaef8
Daniil Lebedkin <lebedkin112@gmail.com>
2021-09-13 12:43:35 +0300

    add preLoader

--- src/index.js
+++ src/index.js
@@ -63,6 +63,8 @@ function asyncLoad(scene){
       nameNum++;
       asyncLoad(scene);
     }else{
+      // preLoader
+      document.getElementById('heroPreloader').remove();
       afterLoad(scene);
     }
   })
